33ad8e21512b79d7a7a5234ffd21ff943c94c295
santoslos <papoian.gosha@gmail.com>
2020-09-24 23:52:50 +0300

    test commit for Ivana

--- bot.py
+++ bot.py
@@ -8,13 +8,13 @@ bot = telebot.TeleBot(TOKEN)
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, я бот по доте start')
 
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
+        bot.send_message(message.chat.id, 'Привет, мой создатель Gosha')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
-
 
 
 bot.polling()
