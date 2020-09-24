615705bd31122df5b3e836e2589cb0beb362307b
santoslos <papoian.gosha@gmail.com>
2020-09-24 23:29:23 +0300

    first bot

---
+++ bot.py
@@ -0,0 +1,20 @@
+import telebot
+
+TOKEN = "1000799565:AAFBwOxONfgDFTyDBf46_6OTSZVpD8Pfc5M"
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, я бот по доте start')
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+
+
+
+bot.polling()
