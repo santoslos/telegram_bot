1dbd6dc29caeeecaecb8835dd3a53baab50e7a0c
santoslos <papoian.gosha@gmail.com>
2020-10-03 01:07:45 +0300

    2th fersion have been created of  dota2 parser

--- parser.py
+++ parser.py
@@ -2,24 +2,41 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-URL = 'https://ru.dotabuff.com/heroes/abaddon'
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
-}
 
-driver = webdriver.Firefox(executable_path=r'/usr/local/bin/geckodriver')
-driver.get("https://ru.dotabuff.com/heroes/abaddon")
-elem = driver.find_element_by_xpath("//div[@class = 'col-8']/section[5]").find_elements_by_tag_name('tr')
-elem_text = []
+def parsers(heroes):
+    elem_text = []
+    elem_text2 = []
+    heroes = heroes.lower()
+    driver = webdriver.Firefox(executable_path=r'/usr/local/bin/geckodriver')
+    driver.get("https://ru.dotabuff.com/heroes/" + heroes)
+    elem_count = driver.find_elements_by_xpath("//div[@class = 'col-8']/section")
 
-i = 0
-a = ''
-elem.pop(0)
-for elemens in elem:
-    elem_cell = []
-    for j in elemens.find_elements_by_tag_name('td'):
-        elem_cell.append(j.text)
-    del elem_cell[0]
-    elem_text.append(elem_cell)
-driver.close()
-print(elem_text)
+    if len(elem_count) == 7:
+
+        elem = driver.find_element_by_xpath("//div[@class = 'col-8']/section[6]").find_elements_by_tag_name('tr')
+        elem2 = driver.find_element_by_xpath("//div[@class = 'col-8']/section[7]").find_elements_by_tag_name('tr')
+    else:
+        elem = driver.find_element_by_xpath("//div[@class = 'col-8']/section[5]").find_elements_by_tag_name('tr')
+        elem2 = driver.find_element_by_xpath("//div[@class = 'col-8']/section[6]").find_elements_by_tag_name('tr')
+
+
+    elem.pop(0)
+    elem2.pop(0)
+
+    for elemens in elem:
+        elem_cell = []
+        for j in elemens.find_elements_by_tag_name('td'):
+            elem_cell.append(j.text)
+        del elem_cell[0]
+        elem_text.append(elem_cell)
+
+    for elemens in elem2:
+        elem_cell = []
+        for j in elemens.find_elements_by_tag_name('td'):
+            elem_cell.append(j.text)
+        del elem_cell[0]
+        elem_text2.append(elem_cell)
+    driver.close()
+    return elem_text, elem_text2
+
+
